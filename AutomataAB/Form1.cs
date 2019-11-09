ef0b4cd61c100ae7effdd638994d9dfa9ad5374f
Mayen <35675641+MayenGames@users.noreply.github.com>
2019-11-08 20:43:15 -0600

    ocultar1

--- AutomataAB/Form1.cs
+++ AutomataAB/Form1.cs
@@ -13,87 +13,25 @@ using System.Text.RegularExpressions;
 using System.Diagnostics;
 
 namespace AutomataAB
-{
-
-    public interface IOperation 
-    {
-        T sum<T>();
-        T rest<T>();
-        T div<T>();
-        T multi<T>();
-    }
-
-    [Serializable]
-    public struct Memory:IOperation
-    { 
-        public string TOKEN { get; set; }
-        public string ID { get; set; }
-        public dynamic VALUE { get; set; }
-        public enum OPlst { sum, res, mult, div }
-        
-        public Memory( string token, string id=null, dynamic value=null) 
-        {
-            TOKEN = token;
-            ID = id;
-            VALUE = value;
-        }
-
-        public static implicit operator decimal (Memory m) => m.VALUE;
-        public static implicit operator string (Memory m) =>m.VALUE;
-
-        public override string ToString()
-        {
-            StringBuilder srt = new StringBuilder();
-            srt.AppendFormat("TOKEN->{0}\nID->{1}\nVALUE->{2}",TOKEN,ID,VALUE);
-            return srt.ToString();
-        }
-
-        public T sum<T>()
-        {
-            throw new NotImplementedException();
-        }
-
-        public T rest<T>()
-        {
-            throw new NotImplementedException();
-        }
-
-        public T div<T>()
-        {
-            throw new NotImplementedException();
-        }
-
-        public T multi<T>()
-        {
-            throw new NotImplementedException();
-        }
-    }
+{ 
     public partial class Form1 : Form {
 
         public string ruta, result;
         string[] palabras;
         public int iter = 0;
-        public delegate void Cons(object str, List<Memory> ls);
-        public Action<bool> algo;
-        public event Cons OnCons;
+        public event Action<object, List<Memory>> OnCons;
         public List<Memory> STACK = new List<Memory>();
-        
+
 
         public static List<string> Tokens = new List<string>();
-        
+
         public static List<string> Identificadores = new List<string>(){
             "Num","Tex","Si","SiTons","Tons","Lop","Sim","Dec","Imp"
         };
-        void prueba(bool a) { }
 
         public Form1() {
-            
+
             InitializeComponent();
-            algo += delegate 
-            {
-
-
-            };
         }
 
         private void exit_Click(object sender, EventArgs e) {
@@ -108,7 +46,7 @@ namespace AutomataAB
                 palabras = inputData.Text.Split(' ');
                 inputMessage.AppendText(inputData.Text + "\n");
                 foreach (var palabra in palabras) {
-                   // result = runAuto(palabra);
+                    // result = runAuto(palabra);
                 }
                 escribir.WriteLine(inputData.Text);
                 escribir.Close();
@@ -132,17 +70,17 @@ namespace AutomataAB
             {
 
                 linea = leer.ReadLine();
-                
+
                 while (linea != null) {
 
                     palabras = linea.Split(' ');
 
-                    for(iter=0; iter<palabras.Length; iter++) {
-                       // result = runAuto(palabras[iter]);
+                    for (iter = 0; iter < palabras.Length; iter++) {
+                        // result = runAuto(palabras[iter]);
                         inputMessage.AppendText(result + "\n");
                     }
                     linea = leer.ReadLine();
-                }                
+                }
             }
             catch {
                 MessageBox.Show("error");
@@ -161,41 +99,15 @@ namespace AutomataAB
 
         private void Form1_Load(object sender, EventArgs e)
         {
-         
+
         }
 
         private void inputData_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
-
-        public async Task<bool> IsConditonal(string Pattern) 
-        {
-            string regex = @"\A\s*((?<token>(Si|SiTons)))\s*\(\s*((?<condition>[a-z]+\s*==\s*('[\s*a-z\s*]*'|[\d+])))\s*\)\s*\{\s*\}$\Z";
-            Regex rgx = new Regex(regex);
-            MatchCollection matchCollection = Regex.Matches(Pattern, regex);
-
-            foreach (Match match in matchCollection) {
-                
-            }
-            return await Task.Run(() => rgx.IsMatch(Pattern)?true:false);
-        }
-        public async Task<bool> IsVar(string Pattern) {
-
-            string regex = @"\A((?<declare>\s*(Num|Dec))\s+(?<id>[a-z]+[0-9]*)\s*(:=(?<value>\s*\d+\s*))?;)*$\Z";         
-            Regex rgx = new Regex(regex);
-            MatchCollection matchCollection = Regex.Matches(Pattern, regex);
-
-            foreach (Match match in matchCollection) 
-            {               
-                string token = match.Groups["declare"].Value.ToString().Replace(" ", null);
-                string id = match.Groups["id"].Value.ToString().Replace(" ", null);
-                dynamic value = match.Groups["value"].Value;
-                Memory variable = new Memory(token, id, value);
-                STACK.Add(variable);
-            }                            
-            return await Task.Run(()=> rgx.IsMatch(Pattern) ? true : false);
-        }
+        
+        
         private async void COMPILAR_Click_1(object sender, EventArgs e)
         {
             var _IsVar= await IsVar(inputMessage.Text) ?"variable valida \n":"variable no valida \n";
---
+++ AutomataAB/Forms/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+
+namespace AutomataAB
+{
+    public interface IOperation
+    {
+        T sum<T>();
+        T rest<T>();
+        T div<T>();
+        T multi<T>();
+    }
+
+    [Serializable]
+    public struct Memory : IOperation
+    {
+        public string TOKEN { get; set; }
+        public string ID { get; set; }
+        public dynamic VALUE { get; set; }
+        public enum OPlst { sum, res, mult, div }
+
+        public Memory(string token, string id = null, dynamic value = null)
+        {
+            TOKEN = token;
+            ID = id;
+            VALUE = value;
+        }
+
+        public static implicit operator decimal(Memory m) => m.VALUE;
+        public static implicit operator string(Memory m) => m.VALUE;
+
+        public override string ToString()
+        {
+            StringBuilder srt = new StringBuilder();
+            srt.AppendFormat("TOKEN->{0}\nID->{1}\nVALUE->{2}", TOKEN, ID, VALUE);
+            return srt.ToString();
+        }
+
+        public T sum<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T rest<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T div<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T multi<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    /// <summary>
+    /// FAKE CLASS
+    /// </summary>
+    partial class Form1 : Form
+    {
+
+        public async Task<bool> IsConditonal(string Pattern)
+        {
+            Regex rgx = new Regex(@"\A\s*((?<token>(Si|SiTons)))\s*\(\s*((?<condition>[a-z]+\s*==\s*('[\s*a-z\s*]*'|[\d+])))\s*\)\s*\{\s*((Num|Dec)\s+([a-z]+[0-9]*)\s*(:=\s*\d+\s*)?;)*\s*\}$\Z");
+            MatchCollection matchCollection = Regex.Matches(Pattern, @"\A\s*((?<token>(Si|SiTons)))\s*\(\s*((?<condition>[a-z]+\s*==\s*('[\s*a-z\s*]*'|[\d+])))\s*\)\s*\{\s*((Num|Dec)\s+([a-z]+[0-9]*)\s*(:=\s*\d+\s*)?;)*\s*\}$\Z");
+
+            foreach (Match match in matchCollection)
+            {
+                string token = match.Groups["token"].Value.ToString().Replace(" ", null);
+                string id = match.Groups["condition"].Value.ToString().Replace(" ", null);
+                //dynamic value = match.Groups["value"].Value;
+                Memory variable = new Memory(token, id);
+                STACK.Add(variable);
+            }
+            return await Task.Run(() => rgx.IsMatch(Pattern) ? true : false);
+        }
+
+        public async Task<bool> IsVar(string Pattern)
+        {
+
+            string varegex = @"\A((?<declare>\s*(Num|Dec))\s+(?<id>[a-z]+[0-9]*)\s*(:=(?<value>\s*\d+\s*))?;)*$\Z";
+            Regex rgx = new Regex(varegex);
+            MatchCollection matchCollection = Regex.Matches(Pattern, varegex);
+
+            foreach (Match match in matchCollection)
+            {
+                string token = match.Groups["declare"].Value.ToString().Replace(" ", null);
+                string id = match.Groups["id"].Value.ToString().Replace(" ", null);
+                dynamic value = match.Groups["value"].Value;
+                Memory variable = new Memory(token, id, value);
+                STACK.Add(variable);
+            }
+            return await Task.Run(() => rgx.IsMatch(Pattern) ? true : false);
+        }
+
+    }
+}
