dfee6579af3c71e6b15e66446d6d6c8aa5723f59
Jose Manuel <manuelvaldez8642@gmail.com>
2019-11-11 12:59:51 -0600

    shit

--- AutomataAB/Form1.Designer.cs
+++ AutomataAB/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.COMPILAR = new System.Windows.Forms.Button();
             this.OpenConsole = new System.Windows.Forms.Button();
+            this.Analizadorxd = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // inputMessage
@@ -47,10 +48,9 @@
             this.inputMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.inputMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.inputMessage.ForeColor = System.Drawing.SystemColors.Window;
-            this.inputMessage.Location = new System.Drawing.Point(205, 34);
-            this.inputMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputMessage.Location = new System.Drawing.Point(308, 52);
             this.inputMessage.Name = "inputMessage";
-            this.inputMessage.Size = new System.Drawing.Size(594, 350);
+            this.inputMessage.Size = new System.Drawing.Size(891, 538);
             this.inputMessage.TabIndex = 5;
             this.inputMessage.Text = "";
             // 
@@ -63,20 +63,18 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(201, 9);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(302, 14);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(108, 20);
+            this.label7.Size = new System.Drawing.Size(157, 29);
             this.label7.TabIndex = 14;
             this.label7.Text = "TU CODIGO";
             // 
             // inputData
             // 
             this.inputData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.inputData.Location = new System.Drawing.Point(11, 91);
-            this.inputData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputData.Location = new System.Drawing.Point(16, 140);
             this.inputData.Name = "inputData";
-            this.inputData.Size = new System.Drawing.Size(179, 20);
+            this.inputData.Size = new System.Drawing.Size(268, 26);
             this.inputData.TabIndex = 2;
             this.inputData.TabStop = false;
             this.inputData.TextChanged += new System.EventHandler(this.inputData_TextChanged);
@@ -86,65 +84,57 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(5, 74);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(8, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(171, 16);
+            this.label2.Size = new System.Drawing.Size(243, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "2.- Ingresa Una Oracion";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(11, 34);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(16, 52);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 24);
+            this.button1.Size = new System.Drawing.Size(267, 37);
             this.button1.TabIndex = 4;
             this.button1.TabStop = false;
             this.button1.Text = "Subir un archivo";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // save
             // 
             this.save.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.save.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.save.Location = new System.Drawing.Point(13, 140);
-            this.save.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.save.Location = new System.Drawing.Point(20, 215);
             this.save.Name = "save";
-            this.save.Size = new System.Drawing.Size(83, 22);
+            this.save.Size = new System.Drawing.Size(124, 34);
             this.save.TabIndex = 6;
             this.save.TabStop = false;
             this.save.Text = "Guardar";
             this.save.UseVisualStyleBackColor = false;
             this.save.Click += new System.EventHandler(this.save_Click);
-            this.save.MouseHover += new System.EventHandler(this.save_MouseHover);
             // 
             // read
             // 
             this.read.BackColor = System.Drawing.Color.MintCream;
             this.read.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.read.Location = new System.Drawing.Point(101, 140);
-            this.read.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.read.Location = new System.Drawing.Point(152, 215);
             this.read.Name = "read";
-            this.read.Size = new System.Drawing.Size(89, 22);
+            this.read.Size = new System.Drawing.Size(134, 34);
             this.read.TabIndex = 7;
             this.read.TabStop = false;
             this.read.Text = "leer";
             this.read.UseVisualStyleBackColor = false;
             this.read.Click += new System.EventHandler(this.read_Click);
-            this.read.MouseHover += new System.EventHandler(this.read_MouseHover);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(8, 17);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(12, 26);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.Size = new System.Drawing.Size(176, 25);
             this.label4.TabIndex = 10;
             this.label4.Text = "1.- Sube un .TxT";
             // 
@@ -153,18 +143,18 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(10, 123);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(15, 189);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(191, 16);
+            this.label5.Size = new System.Drawing.Size(269, 25);
             this.label5.TabIndex = 11;
             this.label5.Text = "3.- Selecciona una opcion.";
             // 
             // COMPILAR
             // 
-            this.COMPILAR.Location = new System.Drawing.Point(13, 183);
+            this.COMPILAR.Location = new System.Drawing.Point(20, 282);
+            this.COMPILAR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.COMPILAR.Name = "COMPILAR";
-            this.COMPILAR.Size = new System.Drawing.Size(176, 23);
+            this.COMPILAR.Size = new System.Drawing.Size(264, 35);
             this.COMPILAR.TabIndex = 16;
             this.COMPILAR.TabStop = false;
             this.COMPILAR.Text = "COMPILAR";
@@ -173,22 +163,32 @@
             // 
             // OpenConsole
             // 
-            this.OpenConsole.Location = new System.Drawing.Point(15, 230);
+            this.OpenConsole.Location = new System.Drawing.Point(22, 354);
+            this.OpenConsole.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.OpenConsole.Name = "OpenConsole";
-            this.OpenConsole.Size = new System.Drawing.Size(174, 23);
+            this.OpenConsole.Size = new System.Drawing.Size(261, 35);
             this.OpenConsole.TabIndex = 17;
             this.OpenConsole.TabStop = false;
             this.OpenConsole.Text = "ABRIR CONSOLA";
             this.OpenConsole.UseVisualStyleBackColor = true;
             this.OpenConsole.Click += new System.EventHandler(this.OpenConsole_Click);
             // 
+            // Analizadorxd
+            // 
+            this.Analizadorxd.Location = new System.Drawing.Point(-13, -45);
+            this.Analizadorxd.Name = "Analizadorxd";
+            this.Analizadorxd.Size = new System.Drawing.Size(10, 38);
+            this.Analizadorxd.TabIndex = 18;
+            this.Analizadorxd.Text = "";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(43)))));
-            this.ClientSize = new System.Drawing.Size(810, 395);
+            this.ClientSize = new System.Drawing.Size(1215, 608);
+            this.Controls.Add(this.Analizadorxd);
             this.Controls.Add(this.OpenConsole);
             this.Controls.Add(this.COMPILAR);
             this.Controls.Add(this.label7);
@@ -200,7 +200,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.inputData);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -222,6 +221,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button COMPILAR;
         private System.Windows.Forms.Button OpenConsole;
+        private System.Windows.Forms.RichTextBox Analizadorxd;
     }
 }
 
--- AutomataAB/Form1.cs
+++ AutomataAB/Form1.cs
@@ -145,5 +145,419 @@ namespace AutomataAB
                 }
             }
         }
+
+        #region  Analizer
+
+        private string runAuto(string word) {
+            Regex reg = new Regex((@"a-zA-Z+"));
+            string finishWord = word, status = "e0", wordNew = "";
+            bool sum = false;
+            char[] l = word.ToCharArray();
+
+            for (int j = 0; j < word.Length;) {
+                switch (status) {
+                    case "e0":
+                        switch (l[j]) {
+                            case '+':
+                                status = "e13";
+                                sum = true;
+                                break;
+                            case '-':
+                                status = "e16";
+                                sum = true;
+                                break;
+                            case '*':
+                                status = "e5";
+                                break;
+                            case ':':
+                                status = "e7";
+                                sum = true;
+                                break;
+                            case ';':
+                                status = "e4";
+                                break;
+                            case '/':
+                                status = "e6";
+                                break;
+                            case '(':
+                                status = "e9";
+                                break;
+                            case ')':
+                                status = "e10";
+                                break;
+                            case '{':
+                                status = "e11";
+                                break;
+                            case '}':
+                                status = "e12";
+                                break;
+                            case '"':
+                                status = "e19";
+                                break;
+                            case ' ':
+                                j = word.Length + 2;
+                                break;
+                            default:
+                                status = "error";
+                                break;
+                        }
+                        if (Regex.IsMatch(l[j].ToString(), @"^[1-9_]+$")) {
+                            status = "e20";
+                            j++;
+                        }
+                        else if (Regex.IsMatch(l[j].ToString(), @"^[a-zA-Z]+$")) {
+                            status = "e1";
+                            wordNew = wordNew + l[j];
+                            j++;
+                        }
+                        if (sum) {
+                            sum = false;
+                            j++;
+                        }
+                        break;
+                    case "e1":
+                        if (Regex.IsMatch(l[j].ToString(), @"^[0-9_]+$")) {
+                            status = "e3";
+                            wordNew = wordNew + l[j];
+                            j++;
+                        }
+                        else if (Regex.IsMatch(l[j].ToString(), @"^[a-zA-Z]+$")) {
+                            status = "e1";
+                            wordNew = wordNew + l[j];
+                            if (j == word.Length - 1) {
+                                status = "e2";
+                            }
+                            else {
+                                j++;
+                            }
+                        }
+                        else {
+                            status = "e2";
+                        }
+                        break;
+                    case "e2":
+                        table(wordNew);
+                        status = "e0";
+                        break;
+                    case "e3":
+                        if (Regex.IsMatch(l[j].ToString(), @"^[a-zA-Z]+$")) {
+                            status = "e1";
+                            wordNew = wordNew + l[j];
+                            j++;
+                        }
+                        else if (Regex.IsMatch(l[j].ToString(), @"^[0-9]+$")) {
+                            status = "e3";
+                            wordNew = wordNew + l[j];
+                            j++;
+                        }
+                        break;
+                    case "e4":
+                        Tokens.Add("Cierre");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e5":
+                        Tokens.Add("Multi");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e6":
+                        Tokens.Add("Div");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e7":
+                        if (l[j] == '=') {
+                            status = "e8";
+                        }
+                        break;
+                    case "e8":
+                        Tokens.Add("asignar");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e9":
+                        Tokens.Add("AbrrPar");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e10":
+                        Tokens.Add("CerrPar");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e11":
+                        Tokens.Add("AbrrLla");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e12":
+                        Tokens.Add("CerrLla");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e13":
+                        if (l[j] == '+') {
+                            status = "e15";
+                        }
+                        else {
+                            status = "e14";
+                        }
+                        break;
+                    case "e14":
+                        Tokens.Add("suma");
+                        status = "e0";
+                        break;
+                    case "e15":
+                        Tokens.Add("incremento");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e16":
+                        if (l[j] == '-') {
+                            status = "e18";
+                        }
+                        else {
+                            status = "e17";
+                        }
+                        break;
+                    case "e17":
+                        Tokens.Add("resta");
+                        status = "e0";
+                        break;
+                    case "e18":
+                        Tokens.Add("decremento");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e19":
+                        Tokens.Add("comilla");
+                        status = "e0";
+                        j++;
+                        break;
+                    case "e20":
+                        if (Regex.IsMatch(l[j].ToString(), @"^[0-9]+$")) {
+                            status = "e20";
+                            if (j == word.Length - 1) {
+                                status = "e21";
+                            }
+                            else {
+                                j++;
+                            }
+                        }
+                        else {
+                            status = "e21";
+                        }
+                        break;
+                    case "e21":
+                        Tokens.Add("Num");
+                        status = "e0";
+                        break;
+                    case "error":
+                        j++;
+                        Tokens.Add("Error");
+                        status = "e0";
+                        break;
+                }
+            }
+            return finishWord;
+        }
+
+        private void table(string word) {
+            bool isWord = true;
+            #region identificadores
+            Identificadores.FindAll(j => {
+
+                if (j == word) {
+                    switch (j) {
+                        case "Num":
+                            isWord = false;
+                            Tokens.Add("int");
+                            break;
+                        case "Tex":
+                            isWord = false;
+                            Tokens.Add("string");
+                            break;
+                        case "Si":
+                            isWord = false;
+                            Tokens.Add("if");
+                            break;
+                        case "Tons":
+                            isWord = false;
+                            Tokens.Add("else");
+                            break;
+                        case "SiTons":
+                            isWord = false;
+                            Tokens.Add("else if");
+                            break;
+                        case "Lop":
+                            isWord = false;
+                            Tokens.Add("while");
+                            break;
+                        case "Sim":
+                            isWord = false;
+                            Tokens.Add("char");
+                            break;
+                        case "Dec":
+                            isWord = false;
+                            Tokens.Add("float");
+                            break;
+                        case "Imp":
+                            isWord = false;
+                            Tokens.Add("imprimir");
+                            break;
+                        default:
+                            Tokens.Add("identificador");
+                            isWord = false;
+                            break;
+                    }
+                    return true;
+                }
+                return false;
+            });
+            if (isWord) {
+                Tokens.Add("identificador");
+            }
+            #endregion
+        }
+
+        private void validator() {
+            // Variables para declarar.
+            string asd = "", asd1 = "", asd2 = "";
+            bool valid = false, dec = true, sent = false;
+            // Variables para asignar.
+            string jkl = "", jkl1 = "", jkl2 = "", jkl3 = "";
+            bool fin = false;
+            Tokens.FindAll(i => {
+                Analizadorxd.AppendText(i + " ");
+                if (jkl == "" && sent == true && i == "identificador")
+                    dec = false;
+                if (sent) {
+                    if (jkl2 != "") {
+                        fin = sentencia(jkl, jkl1, jkl2) && i == "Cierre" ? true : false;
+                        jkl = "";
+                        jkl1 = "";
+                        jkl2 = "";
+                        Analizadorxd.AppendText(fin + "\n\n");
+                    }
+                    jkl2 = jkl2 == "" && jkl1 != "" ? i : jkl2;
+                    jkl1 = jkl1 == "" && jkl != "" ? i : jkl1;
+                    if (i == "identificador" && jkl == "")
+                        jkl = i;
+                }
+                if (dec) {
+                    asd2 = asd2 == "" && asd1 != "" ? i : asd2;
+                    asd1 = asd1 == "" && asd != "" ? i : asd1;
+                    if (i == "int" || i == "string") {
+                        asd = i;
+                        asd1 = "";
+                        asd2 = "";
+                    }
+                    else if (asd == "") {
+                        Analizadorxd.AppendText(" Error en la linea" + "\n");
+                    }
+                    if (asd2 != "") {
+                        valid = declaraciones(asd, asd1, asd2);
+                        asd = "";
+                        asd1 = "";
+                        asd2 = "";
+                        Analizadorxd.AppendText("\n");
+                        if (valid == false)
+                            Analizadorxd.AppendText(" Error en la linea" + "\n");
+                        else
+                            sent = true;
+                    }
+                }
+                return true;
+            });
+            if (valid == false)
+                Analizadorxd.AppendText(" Error en la linea" + "\n");
+
+            bool declaraciones(string tipo, string ident, string cierre) {
+                bool _tipo, _ident, _cierre;
+                _tipo = tipo == "int" || tipo == "string" ? true : false;
+                _ident = ident == "identificador" && _tipo ? true : false;
+                _cierre = cierre == "Cierre" && _ident == true ? true : false;
+                return _cierre;
+            }
+
+            bool sentencia(string ide, string asig, string var) {
+                bool _ide, _asig, _var;
+                _ide = ide == "identificador" ? true : false;
+                _asig = asig == "asignar" && _ide ? true : false;
+                _var = ide == "identificador" && _asig ? true : false;
+                return _var == true ? true : false;
+            }
+        }
+        private void Sintactico() {
+            int turn = 0;
+            Tokens.FindAll(token => {
+                switch (token) {
+                    case "identificador":
+                        if (turn == 0 || turn == 2) {
+                            Analizadorxd.AppendText(token + " ");
+                            turn++;
+                        }
+                        else {
+                            Analizadorxd.AppendText("Error ");
+                        }
+                        break;
+                    case "asignar":
+                        if (turn == 1) {
+                            Analizadorxd.AppendText(token + " ");
+                            turn++;
+                        }
+                        else {
+                            Analizadorxd.AppendText("Error ");
+                        }
+                        break;
+                    case "Num":
+                        if (turn == 2) {
+                            Analizadorxd.AppendText(token + " ");
+                            turn++;
+                        }
+                        else {
+                            Analizadorxd.AppendText("Error ");
+                        }
+                        break;
+                    case "Cierre":
+                        Analizadorxd.AppendText(token + "\n");
+                        turn = 0;
+                        break;
+                }
+                return true;
+            });
+        }
+
+        private void save_MouseHover2(object sender, EventArgs e) {
+            toolTip1.ToolTipTitle = "Guardar";
+            toolTip1.Show("Guardar una palabra en un archivo .TxT", save);
+        }
+
+        private void read_MouseHover2(object sender, EventArgs e) {
+            toolTip1.ToolTipTitle = "Leer";
+            toolTip1.Show("Muestra todas las oraciones de un .TxT", read);
+        }
+
+        private void button1_Click2(object sender, EventArgs e) {
+            using (var open = new OpenFileDialog()) {
+                open.Filter = "Archivos txt(*.txt)|*.txt";
+                open.Title = "Archivos txt";
+                if (open.ShowDialog() == DialogResult.OK) {
+                    ruta = open.FileName;
+                    inputMessage.AppendText(ruta);
+                }
+            }
+        }
+
+        #endregion
+
+
+
+
+
+
     }
+
 }
+
